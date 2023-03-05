694d6369636a53a17fe21efde24af0f437801422
Leonardo <leonardosarto1@hotmail.com>
2023-03-05 09:28:36 -0300

    Criação do portfolio

---
+++ index.js
@@ -0,0 +1,11 @@
+function scrollToContact() {
+    document.getElementById("contato").scrollIntoView();
+}
+
+function scrollToProjects() {
+    document.getElementById("projeto").scrollIntoView();
+}
+
+function scrollToConhecimento() {
+    document.getElementById("experiencia").scrollIntoView();
+}
